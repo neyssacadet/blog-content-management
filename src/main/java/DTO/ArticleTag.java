756f597c67b499df1c50006729ef959d42cf507a
neyssacadet <neyssacadet@gmail.com>
2022-12-05 12:21:48 -0600

    added ArticleTag private variables

--- src/main/java/DTO/ArticleTag.java
+++ src/main/java/DTO/ArticleTag.java
@@ -1,4 +1,22 @@
 package DTO;
 
 public class ArticleTag {
+    private int articleID;
+    private int tagID;
+
+    public int getArticleID() {
+        return articleID;
+    }
+
+    public void setArticleID(int articleID) {
+        this.articleID = articleID;
+    }
+
+    public int getTagID() {
+        return tagID;
+    }
+
+    public void setTagID(int tagID) {
+        this.tagID = tagID;
+    }
 }
