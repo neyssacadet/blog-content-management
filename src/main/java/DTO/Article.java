59f76537212708e29290d29eff45bdba5b6d28f0
neyssacadet <neyssacadet@gmail.com>
2022-12-05 12:22:03 -0600

    added Article private variables

--- src/main/java/DTO/Article.java
+++ src/main/java/DTO/Article.java
@@ -1,4 +1,69 @@
 package DTO;
 
+import java.util.Date;
+
 public class Article {
+    private int articleID;
+    private String title;
+    private String body;
+    private String author;
+    private Date createdOn;
+    private Date postOn;
+    private Date expiredOn;
+
+    public int getArticleID() {
+        return articleID;
+    }
+
+    public void setArticleID(int articleID) {
+        this.articleID = articleID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getPostOn() {
+        return postOn;
+    }
+
+    public void setPostOn(Date postOn) {
+        this.postOn = postOn;
+    }
+
+    public Date getExpiredOn() {
+        return expiredOn;
+    }
+
+    public void setExpiredOn(Date expiredOn) {
+        this.expiredOn = expiredOn;
+    }
 }
