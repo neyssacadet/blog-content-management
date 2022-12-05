0d2da60d3aeca619e2d4486df2f3c45d9f5e936e
Everlyn <eveeleon@gmail.com>
2022-12-05 08:40:15 -0800

    SQL is finished

---
+++ BlogContentDBScript.sql
@@ -0,0 +1,67 @@
+DROP DATABASE IF EXISTS blogContentDBScript;
+CREATE DATABASE blogContentDBScript;
+
+USE blogContentDBScript;
+
+CREATE TABLE User (
+    	userID INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    isEnabled BIT NOT NULL DEFAULT 0,
+    email VARCHAR(50),
+    createdOn DATETIME NOT NULL DEFAULT NOW()
+);
+
+
+CREATE TABLE Article (
+    	articleID INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    createdOn DATETIME NOT NULL DEFAULT NOW(),
+    postOn DATETIME NOT NULL,
+    expireOn DATETIME NOT NULL
+);
+
+CREATE TABLE Tag (
+	tagID INT PRIMARY KEY AUTO_INCREMENT,
+    tagName VARCHAR(150) NOT NULL UNIQUE,
+    createdBy VARCHAR(255) NOT NULL,
+    addedOn VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE ArticleTag (
+  	articleID INT NOT NULL,
+  tagID INT NOT NULL,
+  PRIMARY KEY (articleID, tagID),
+  CONSTRAINT PK_ArticleTag
+    FOREIGN KEY (articleID)
+    REFERENCES Article(articleID),
+    FOREIGN KEY (tagID)
+    REFERENCES Tag(tagID)
+);
+
+
+CREATE TABLE Comments (
+	commentsID INT PRIMARY KEY AUTO_INCREMENT,
+    text TEXT NOT NULL,
+    user VARCHAR(255) NOT NULL,
+    userEmail VARCHAR(255) NOT NULL,
+    addedOn VARCHAR(50) NOT NULL,
+    articleID INT NOT NULL,
+    FOREIGN KEY (articleID) 
+    REFERENCES Article(articleID)
+);
+
+CREATE TABLE ArticleComments (
+  	articleID INT NOT NULL,
+  commentsID INT NOT NULL,
+  PRIMARY KEY (articleID, commentsID),
+  CONSTRAINT PK_ArticleComments
+    FOREIGN KEY (articleID)
+    REFERENCES Article(articleID),
+    FOREIGN KEY (commentsID)
+    REFERENCES Comments(commentsID)
+);
