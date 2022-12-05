ed85a1ed7e6885b813eb10f512ef73e9f0f021a1
neyssacadet <neyssacadet@gmail.com>
2022-12-05 12:17:25 -0600

    added User private variables

--- src/main/java/DTO/User.java
+++ src/main/java/DTO/User.java
@@ -1,4 +1,14 @@
 package DTO;
 
+import java.util.Date;
+
 public class User {
+    private int userID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private boolean isEnabled;
+    private String email;
+    private Date createdOn;
 }
