eb388a87d12a932b06fcd1dfb793641e496bbbf0
malakhavam <malakhava@yahoo.com>
2022-12-06 12:56:22 -0500

    added security

---
+++ blog-content-management/src/main/java/com/example/blogcontentmanagement/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.blogcontentmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ *
+ * @author 18437
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled =true)
+public class WebSecurityConfig {
+    
+    private static final String[] WHITELIST = {
+        "/"
+    };
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(WHITELIST).permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+}
--- blog-content-management/src/main/java/com/example/blogcontentmanagement/models/Article.java
+++ blog-content-management/src/main/java/com/example/blogcontentmanagement/models/Article.java
@@ -18,9 +18,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 /**
  *
